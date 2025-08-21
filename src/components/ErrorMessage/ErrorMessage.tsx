468945ee28237fec9fb18fad7cd717434a9d5531
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-21 03:05:51 +0300

    add some code for test

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import styles from "./ErrorMessage.module.css";
+
+const ErrorMessage = (): JSX.Element => {
+    return (
+        <p className={styles.text}>There was an error, please try again...</p>
+    );
+};
+
+export default ErrorMessage;
